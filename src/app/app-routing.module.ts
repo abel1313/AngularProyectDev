ef98b6a886d486ef2cc739f2223105552cb1f837
trece <abeltrece@gmail.com>
2021-10-11 12:35:29 -0700

    Crear estructura de proyecto angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = 
+[
+  {
+    path: 'clientes',
+    loadChildren: () => import('../app/views/cliente-view/cliente-view.module')
+    .then(cliente => cliente.ClienteViewModule)
+  },
+  {
+    path: 'productos',
+    loadChildren: () => import('../app/views/producto-view/producto-view.module')
+    .then(producto => producto.ProductoViewModule)
+  },
+  {
+    path: 'usuarios',
+    loadChildren: () => import('../app/views/usuario-view/usuario-view.module')
+    .then(usuario => usuario.UsuarioViewModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
